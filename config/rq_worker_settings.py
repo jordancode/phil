049371b446dbe0568b3d5f59e49b97cbfd494df1
Jordan Claassen <jordan@Jordans-MacBook-Pro.local>
2016-02-23 14:43:01 -0800

    add script to pull worker settings from json

---
+++ config/rq_worker_settings.py
@@ -0,0 +1,20 @@
+from framework.config.config import Config
+
+"""
+    Exports config variables for rq worker to read.
+    
+    Usage:
+        cd /srv/www/photo_app 
+        export ENV=<DEV|STAGE|PROD>
+        rq worker -c framework.config.rq_worker_settings
+"""
+
+connection_config = Config.get("redis", "connection")
+dbs_config = Config.get("redis", "dbs")
+
+REDIS_HOST = connection_config.get("host")
+REDIS_PORT = connection_config.get("port")
+
+REDIS_DB = dbs_config.get("queue")
+
+QUEUES = ['default']
