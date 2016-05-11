460c79f63e9e32d31b2c0359bf2853f2532f8fee
Jordan Claassen <jordan@Jordans-MacBook-Pro.local>
2016-05-10 19:25:50 -0700

    cache session

--- http/cookie_session_store.py
+++ http/cookie_session_store.py
@@ -9,6 +9,9 @@ class CookieSessionStore(BaseSessionStore):
     
     COOKIE_MAX_AGE=630720000
     
+    
+    _session=None
+    
     def _get_cookie_name(self):
         app_name = Config.get("app","app_key")
         
@@ -24,6 +27,8 @@ class CookieSessionStore(BaseSessionStore):
         return "." + server_name
     
     def set_session(self, response, session):
+        
+        self._session = session
         
         max_expires_ts = datetime.datetime.now() + datetime.timedelta(seconds=self.COOKIE_MAX_AGE)
         if not session.log_out_ts:
@@ -43,21 +48,27 @@ class CookieSessionStore(BaseSessionStore):
         #response.set_cookie(self._get_token_cookie_name(), session.token, None, session.log_out_ts, '/')
     
     def get_session(self, request):
-                
-        logging.getLogger().debug("TRY GET SESSION!")
-        logging.getLogger().debug("Cookies:  " + repr(request.cookies))
-        try:
-            session_id = request.cookies[self._get_cookie_name()]
-            token = request.cookies[self._get_token_cookie_name()]
-        except KeyError as e:
-            logging.getLogger().debug("No session cookie :(")
-            raise framework.models.session.NoActiveSessionException()
         
-        return  framework.models.session.SessionService().get_active_session(int(session_id), token)
+        if not self._session:
+                
+            logging.getLogger().debug("TRY GET SESSION!")
+            logging.getLogger().debug("Cookies:  " + repr(request.cookies))
+            try:
+                session_id = request.cookies[self._get_cookie_name()]
+                token = request.cookies[self._get_token_cookie_name()]
+            except KeyError as e:
+                logging.getLogger().debug("No session cookie :(")
+                raise framework.models.session.NoActiveSessionException()
+            
+            self._session =  framework.models.session.SessionService().get_active_session(int(session_id), token)
+        
+        return self._session
         
     
     
     def delete_session(self, response):
+        
+        self._session = None
         
         response.delete_cookie(self._get_cookie_name(),'/', self._get_cookie_domain())
         response.delete_cookie(self._get_token_cookie_name(),'/', self._get_cookie_domain())
