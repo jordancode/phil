e6a935378507eab73ff540bdf07c59e7fc9d7710
Jordan Claassen <jordan@Jordans-MacBook-Pro.local>
2016-06-30 09:59:29 -0700

    add context to stringification, add methods to tokens

--- utils/json_utils.py
+++ utils/json_utils.py
@@ -3,31 +3,31 @@ import datetime
 import json
 class JSONUtils:
     
-    _optional_keys = None
-    
     @classmethod
-    def dumps(cls, dict_or_array, optional_keys = None):
-        
-        cls._optional_keys = optional_keys
-        return json.dumps(dict_or_array, sort_keys=True, default=cls._json_helper)
-    
-    
-    @classmethod
-    def _json_helper(cls, value):
-        
-        #stringifies otherwise non-json nodes
-        try:
-            return value.to_dict(True, cls._optional_keys)
-        except AttributeError:
-            pass
+    def dumps(cls, dict_or_array, optional_keys = None, context=None, stringify_remainder=True):
         
         
-        try:
-            if isinstance(value, datetime.datetime):
-                return DateUtils.datetime_to_unix(value)
+        def json_helper(value):
+            #stringifies otherwise non-json nodes
+            try:
+                value.set_context(context)
+                return value.to_dict(True, optional_keys)
+            except AttributeError:
+                pass
             
-            return str(value)
-        except AttributeError:
-            pass
+            try:
+                if isinstance(value, datetime.datetime):
+                    return DateUtils.datetime_to_unix(value)
+                
+                if stringify_remainder:
+                    return str(value)
+            except AttributeError:
+                pass
+            
+            raise TypeError()
+
         
-        raise TypeError()
+        
+        return json.dumps(dict_or_array, sort_keys=True, default=json_helper)
+    
+    
