c515ed7df821735fa538d3782cb174e564a89de4
Jordan Claassen <jordan@Jordans-MacBook-Pro.local>
2016-07-11 17:58:14 -0700

    update stats tracker to take a user agent

--- storage/stats_tracker.py
+++ storage/stats_tracker.py
@@ -2,15 +2,19 @@ import hashlib
 import logging
 
 from statsd.client import TCPStatsClient
+from user_agents import parse
 
 from framework.config.config import Config
 
 
 class StatsTracker(TCPStatsClient):
     
-    def __init__(self):
+    _ua_string=None
+    
+    def __init__(self, ua_string=None):
         config = Config.get("statsd")
         super().__init__(**config)
+        self._ua_string=None
     
     
     def _send(self, data):
@@ -21,11 +25,16 @@ class StatsTracker(TCPStatsClient):
             self.close()
     
     def track(self, event, count=1):
-        return self.incr(event, count)
+        
+        self.incr(event, count)
+        self._incr_platform(event, count, 1)
+        
     
     
     def incr(self, stat, count=1, rate=1):
-        return super().incr(stat,count,rate)
+        super().incr(stat,count,rate)
+        self._incr_platform(stat,count,rate)
+        
         
     
     def verify_checksum(self, event, count, sample, checksum):
@@ -35,5 +44,25 @@ class StatsTracker(TCPStatsClient):
         key = (str(event) + ":" + str(count) + ":" + str(sample)).encode("utf-8")
         
         return hashlib.md5(key).hexdigest().lower() == checksum.lower()
+    
+    
+    def _incr_platform(self, event, count=1, rate=1):
+        if not self._ua_string:
+            return
+        
+        family = parse(self._ua_string).os.family
+        
+        if family == "Android":
+            event = event+"._android"
+        elif family == "iOS":
+            event = event+"._ios"
+        else:
+            event = event+"._desktop"
+        
+        return super().incr(event, count, rate)
+            
+    
+            
+        
         
         
