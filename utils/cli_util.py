16131ed33b65e70d324bb859fa68e8ae832ce191
Jordan Claassen <jordan@Jordans-MacBook-Pro.local>
2016-11-29 13:22:06 -0800

    add util to get a command line arg from a ps response

--- utils/cli_util.py
+++ utils/cli_util.py
@@ -17,3 +17,16 @@ class CLIUtil:
                     return arg[len(short_prefix):]
         
         return None
+    
+    @staticmethod
+    def get_named_arg_from_ps_line(ps_line, name, short_name=None):
+        keys=["--"+name+"="]
+        if short_name:
+            keys.append("-"+short_name+"=")
+        
+        for k in keys:
+            if k in ps_line:
+                values_part = ps_line.split(k)[1]
+                return values_part.split(" ")[0] 
+        
+        return None
